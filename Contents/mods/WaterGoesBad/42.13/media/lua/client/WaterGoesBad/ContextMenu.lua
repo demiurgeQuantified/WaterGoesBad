1121ba193064957083c31f620057b75bf41e47db
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-01 18:26:33 +0000

    move add filter option into object submenu

--- Contents/mods/WaterGoesBad/42.13/media/lua/client/WaterGoesBad/ContextMenu.lua
+++ Contents/mods/WaterGoesBad/42.13/media/lua/client/WaterGoesBad/ContextMenu.lua
@@ -2,6 +2,9 @@ local ChangeTapFilterAction = require("WaterGoesBad/ChangeTapFilterAction")
 local Utils = require("WaterGoesBad/Utils")
 
 
+local DRINK_TEXT = getText("ContextMenu_Drink")
+
+
 local ContextMenu = {}
 
 
@@ -13,10 +16,63 @@ local function predicateNotBroken(item)
 end
 
 
----@param itemToPipe IsoObject
+---@param object IsoObject
+function ContextMenu.isFilterable(object)
+	return object:hasProperty(IsoFlagType.waterPiped) and object:getUsesExternalWaterSource()
+end
+
+
+---@param object IsoObject
+---@return boolean
+---@nodiscard
+local function shouldBeTainted(object)
+	return not Utils.hasFilter(object) and object:getUsesExternalWaterSource() and object:FindExternalWaterSource():isTaintedWater()
+end
+
+
+---Adds tainted water text to an option's tooltip
+---@param option umbrella.ISContextMenu.Option
+local function addTaintedWaterTooltip(option)
+	assert(option.toolTip ~= nil, "Drink option has no tooltip")
+
+	option.toolTip.description = string.format(
+		"%s <BR> <RGB:1,0.5,0.5> %s",
+		option.toolTip.description,
+		Translator.getText("Tooltip_item_TaintedWater")
+	)
+end
+
+
+---@param context ISContextMenu
+---@return table<IsoObject, ISContextMenu>
+local function findSinkSubmenus(context)
+	---@type table<IsoObject, ISContextMenu>
+	local submenuMap = {}
+
+	for i = 1, #context.options do
+		local option = context.options[i]
+		if option.subOption then
+			local submenu = context:getSubInstance(option.subOption)
+			assert(submenu ~= nil)
+			local drinkOption = submenu:getOptionFromName(DRINK_TEXT)
+			if drinkOption then
+				local object = drinkOption.param3
+				if instanceof(object, "IsoObject") then
+					---@cast object IsoObject
+					submenuMap[object] = submenu
+				end
+			end
+		end
+	end
+
+	return submenuMap
+end
+
+
+---@param sink IsoObject
 ---@param player IsoPlayer
 ---@param isAddFilter boolean
-function ContextMenu.onFilterOptionPressed(itemToPipe, player, isAddFilter)
+function ContextMenu.onFilterOptionPressed(sink, player, isAddFilter)
 	local wrench = player:getInventory():getFirstTagEvalRecurse(
 		ItemTag.PIPE_WRENCH,
 		predicateNotBroken
@@ -32,7 +88,7 @@ function ContextMenu.onFilterOptionPressed(itemToPipe, player, isAddFilter)
 	ISTimedActionQueue.add(
 		ChangeTapFilterAction:new(
 			player,
-			itemToPipe,
+			sink,
 			wrench,
 			isAddFilter
 		)
@@ -40,110 +96,58 @@ function ContextMenu.onFilterOptionPressed(itemToPipe, player, isAddFilter)
 end
 
 
+---Adds an add/remove filter context option if appropriate.
+---@param context ISContextMenu
 ---@param object IsoObject
-function ContextMenu.isFilterable(object)
-	return object:hasProperty(IsoFlagType.waterPiped) and object:getUsesExternalWaterSource()
-end
-
-
----Marks a Drink option as tainted if the sink is plumbed to a tainted water source.
----@param option umbrella.ISContextMenu.Option
-local function updateDrinkOption(option)
-	if option.toolTip == nil then
-		return
-	end
-
-	local object = option.param3
-	if not instanceof(object, "IsoObject") then
-		-- we are writing defensively here because we search for these by name
-		-- which could easily hit something else by some mod
-		return
-	end
-	---@cast object IsoObject
-
-	if not Utils.hasFilter(object) and object:getUsesExternalWaterSource() and object:FindExternalWaterSource():isTaintedWater() then
-		option.toolTip.description = string.format(
-			"%s <BR> <RGB:1,0.5,0.5> %s",
-			option.toolTip.description,
-			Translator.getText("Tooltip_item_TaintedWater")
-		)
-	end
-end
-
-
----Marks Drink options as tainted for sinks that are plumbed to a tainted water source.
----@param context ISContextMenu
-local function updateTaintedWaterTooltips(context)
-	local DRINK_TEXT = getText("ContextMenu_Drink")
-
-	for i = 1, #context.options do
-		local option = context.options[i]
-		if option.subOption then
-			local submenu = context:getSubInstance(option.subOption)
-			assert(submenu ~= nil)
-			local drinkOption = submenu:getOptionFromName(DRINK_TEXT)
-			if drinkOption then
-				updateDrinkOption(drinkOption)
-			end
-		end
-	end
-end
-
-
----@param context ISContextMenu
 ---@param player IsoPlayer
----@param objects IsoObject[]
-local function addFilterContextOption(context, player, objects)
-	for i = 1, #objects do
-		local object = objects[i]
-		if ContextMenu.isFilterable(object) then
-			local inventory = player:getInventory()
-			local playerHasFilter = inventory:containsType("WaterGoesBad.TapFilter")
-			local playerHasWrench = inventory:containsTagEvalRecurse(
-				ItemTag.PIPE_WRENCH,
-				predicateNotBroken
+local function doFilterContextOption(context, object, player)
+	if ContextMenu.isFilterable(object) then
+		local inventory = player:getInventory()
+		local playerHasFilter = inventory:containsType("WaterGoesBad.TapFilter")
+		local playerHasWrench = inventory:containsTagEvalRecurse(
+			ItemTag.PIPE_WRENCH,
+			predicateNotBroken
+		)
+
+		if not (playerHasFilter or playerHasWrench) then
+			return
+		end
+
+		local hasFilter = Utils.hasFilter(object)
+		local name = getText(
+			hasFilter and "ContextMenu_WaterGoesBad_RemoveFilter" or "ContextMenu_WaterGoesBad_AddFilter",
+			-- leaving this extra argument here because the italian translation is not updated and will still use it
+			Utils.getMoveableDisplayName(object)
+		)
+
+		local option = context:addOption(
+			name,
+			object,
+			ContextMenu.onFilterOptionPressed,
+			player,
+			not hasFilter
+		)
+
+		local tooltip
+		if not (hasFilter or playerHasFilter) then
+			tooltip = ISWorldObjectContextMenu.addToolTip()
+			tooltip:setName(name)
+			tooltip.description = getText(
+				"Tooltip_NeedWrench",
+				getItemNameFromFullType("WaterGoesBad.TapFilter")
 			)
-
-			if not (playerHasFilter or playerHasWrench) then
-				return
-			end
-
-			local modData = object:getModData().WaterGoesBad
-			local hasFilter = modData and modData.hasTapFilter
-			local name = getText(
-				hasFilter and "ContextMenu_WaterGoesBad_RemoveFilter" or "ContextMenu_WaterGoesBad_AddFilter",
-				Utils.getMoveableDisplayName(object)
+		elseif not playerHasWrench then
+			tooltip = ISWorldObjectContextMenu.addToolTip()
+			tooltip:setName(name)
+			tooltip.description = getText(
+				"Tooltip_NeedWrench",
+				getItemNameFromFullType(ItemKey.Weapon.PIPE_WRENCH:toString())
 			)
+		end
 
-			local option = context:addOption(
-				name,
-				object,
-				ContextMenu.onFilterOptionPressed,
-				player,
-				not hasFilter
-			)
-
-			local tooltip
-			if not (hasFilter or playerHasFilter) then
-				tooltip = ISWorldObjectContextMenu.addToolTip()
-				tooltip:setName(name)
-				tooltip.description = getText(
-					"Tooltip_NeedWrench",
-					getItemNameFromFullType("WaterGoesBad.TapFilter")
-				)
-			elseif not playerHasWrench then
-				tooltip = ISWorldObjectContextMenu.addToolTip()
-				tooltip:setName(name)
-				tooltip.description = getText(
-					"Tooltip_NeedWrench",
-					getItemNameFromFullType(ItemKey.Weapon.PIPE_WRENCH:toString())
-				)
-			end
-
-			if tooltip then
-				option.notAvailable = true
-				option.toolTip = tooltip
-			end
+		if tooltip then
+			option.notAvailable = true
+			option.toolTip = tooltip
 		end
 	end
 end
@@ -153,31 +157,32 @@ end
 ---@param context ISContextMenu
 ---@param worldObjects IsoObject[]
 ---@param test boolean
-function ContextMenu.addFilterContextOption(playerNum, context, worldObjects, test)
+function ContextMenu.updateContextMenu(playerNum, context, worldObjects, test)
 	---@diagnostic disable-next-line: undefined-field
-	if test or not SandboxVars.WaterGoesBad.NeedFilterWater then
+	if not SandboxVars.WaterGoesBad.NeedFilterWater then
 		return
 	end
 
-	if SandboxVars.EnableTaintedWaterText then
-		updateTaintedWaterTooltips(context)
-	end
+	local submenus = findSinkSubmenus(context)
+	for sink, submenu in pairs(submenus) do
+		doFilterContextOption(
+			submenu,
+			sink,
+			getSpecificPlayer(playerNum)
+		)
 
-	-- not sure if this ever actually happens, but just in case
-	if #worldObjects < 1 then
-		return
+		if SandboxVars.EnableTaintedWaterText then
+			local drinkOption = submenu:getOptionFromName(DRINK_TEXT)
+			-- currently this option is always present because findSinkSubmenus searches for drink options
+			--  but in the future maybe there'll be things to filter that you can't directly drink from
+			if drinkOption and shouldBeTainted(sink) then
+				addTaintedWaterTooltip(drinkOption)
+			end
+		end
 	end
-	---@diagnostic disable-next-line: need-check-nil
-	local objects = worldObjects[1]:getSquare():getLuaTileObjectList()
-
-	addFilterContextOption(
-		context,
-		getSpecificPlayer(playerNum),
-		objects
-	)
 end
 
-Events.OnFillWorldObjectContextMenu.Add(ContextMenu.addFilterContextOption)
+Events.OnFillWorldObjectContextMenu.Add(ContextMenu.updateContextMenu)
 
 
 return ContextMenu
