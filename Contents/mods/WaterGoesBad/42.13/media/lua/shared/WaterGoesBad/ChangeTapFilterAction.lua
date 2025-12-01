ade993cb969b3687c24e02a7a408faf0fcc7031b
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-01 04:38:28 +0000

    flatten ISPlumbItem into ChangeTapFilter as inheritance is not really meaningful here

--- Contents/mods/WaterGoesBad/42.13/media/lua/shared/WaterGoesBad/ChangeTapFilterAction.lua
+++ Contents/mods/WaterGoesBad/42.13/media/lua/shared/WaterGoesBad/ChangeTapFilterAction.lua
@@ -1,20 +1,39 @@
----@class ChangeTapFilterAction : ISPlumbItem
----@field character IsoGameCharacter
+---@class ChangeTapFilterAction : ISBaseTimedAction
 ---@field modData table
 ---@field isAddFilter boolean
-local ChangeTapFilterAction = ISPlumbItem:derive("WaterGoesBadChangeTapFilterAction")
+---@field wrench InventoryItem
+---@field tap IsoObject
+---@field sound integer
+local ChangeTapFilterAction = ISBaseTimedAction:derive("WaterGoesBadChangeTapFilterAction")
 ChangeTapFilterAction.__index = ChangeTapFilterAction
 
 
 function ChangeTapFilterAction:isValid()
 	return not ((self.modData.hasTapFilter == true) == self.isAddFilter)
-			and ISPlumbItem.isValid(self)
+			and self.character:isEquipped(self.wrench)
+end
+
+
+function ChangeTapFilterAction:stopSound()
+	self.character:stopOrTriggerSound(self.sound)
+end
+
+
+function ChangeTapFilterAction:stop()
+	self:stopSound()
+	ISBaseTimedAction.stop(self)
+end
+
+
+function ChangeTapFilterAction:perform()
+	self:stopSound()
+	ISBaseTimedAction.perform(self)
 end
 
 
 function ChangeTapFilterAction:complete()
 	self.modData.hasTapFilter = self.isAddFilter
-	self.itemToPipe:transmitModData()
+	self.tap:transmitModData()
 
 	local inventory = self.character:getInventory()
 	if self.isAddFilter then
@@ -25,26 +44,49 @@ function ChangeTapFilterAction:complete()
 		sendAddItemToContainer(inventory, item)
 	end
 
-	buildUtil.setHaveConstruction(self.itemToPipe:getSquare(), true)
+	buildUtil.setHaveConstruction(self.tap:getSquare(), true)
 
 	return true
 end
 
 
+function ChangeTapFilterAction:update()
+	self.character:faceThisObject(self.tap)
+
+    self.character:setMetabolicTarget(Metabolics.MediumWork);
+end
+
+
+function ChangeTapFilterAction:start()
+	self.sound = self.character:playSound("RepairWithWrench")
+end
+
+
+function ChangeTapFilterAction:getDuration()
+	if self.character:isTimedActionInstant() then
+		return 1
+	end
+	return 100
+end
+
+
 ---@param character IsoGameCharacter
----@param itemToPipe IsoObject
+---@param tap IsoObject
 ---@param wrench InventoryItem
 ---@param isAddFilter boolean
 ---@return self
 ---@nodiscard
-function ChangeTapFilterAction:new(character, itemToPipe, wrench, isAddFilter)
-	local o = ISPlumbItem.new(self, character, itemToPipe, wrench) ---@as ChangeTapFilterAction
+function ChangeTapFilterAction:new(character, tap, wrench, isAddFilter)
+	local o = ISBaseTimedAction.new(self, character) ---@as ChangeTapFilterAction
 
-	local modData = itemToPipe:getModData()
+	local modData = tap:getModData()
 	modData.WaterGoesBad = modData.WaterGoesBad or {}
 
 	o.modData = modData.WaterGoesBad
 	o.isAddFilter = isAddFilter
+	o.tap = tap
+	o.wrench = wrench
+	o.sound = -1
 
 	o.maxTime = o:getDuration()
 
